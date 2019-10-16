f7ecf1e1b7331be2608f948584a49828c34cc113
sangjin1993 <50225008+sangjin1993@users.noreply.github.com>
2019-10-17 02:45:52 +0900

    change Language

--- gretting.js
+++ gretting.js
@@ -25,7 +25,7 @@ function askForName() {
 function paintGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
-    greeting.innerText = `Hello ${text}`;
+    greeting.innerText = `こんにちは ${text}さん`;
 }
 
 function loadName() {
